08b887e0d4315883cf5be52af9679edb6cb1e9e8
koksraster <70954215+koksraster@users.noreply.github.com>
2022-02-13 18:07:05 +0300

    Create binary_search.py

---
+++ Python/binary_search.py
@@ -0,0 +1,35 @@
+def binary_search(lst,x):
+	start = 0
+	finish = len(lst)-1
+	attemp = 0
+
+	while start <= finish:
+		mid = (start+finish)//2
+		guess = lst[mid]
+		attemp += 1
+
+		if guess == x:
+			answer = "Число отгаданно за " + str(attemp) + " попыток. Число - " + str(guess)
+			return answer
+
+		elif guess > x:
+			finish = mid-1
+
+		else:
+			start = mid + 1
+	return None
+
+try:
+	count_one = int(input("Введите диапозон поиска:\n"))
+	x = int(input("Введите искомое число:\n"))
+except ValueError as error:
+	print("Ошибка -", error)
+	count_one = 100
+	x = 0
+
+lst = [i for i in range(1, (count_one + 1))]
+
+b_s = binary_search(lst,x)
+print(b_s)
+
+input()
